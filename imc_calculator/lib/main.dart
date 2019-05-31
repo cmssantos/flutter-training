ab1de84f2acb111a4b418f81d9427bd86cb6762b
Cleudice Marques Santos <cleudice.santos@mega.com.br>
2019-05-30 21:32:12 -0300

    Update project imc_calculator

--- imc_calculator/lib/main.dart
+++ imc_calculator/lib/main.dart
@@ -12,6 +12,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _infoText = "Informe seus dados!";
+
+  void _resetFields() {
+    _formKey.currentState.reset();
+    weightController.text = "";
+    heightController.text = "";
+    setState(() {
+      _infoText = "Informe seus dados!";
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      String resultIMC = imc.toStringAsPrecision(4);
+
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso ($resultIMC)";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso ideal ($resultIMC)";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente acima do peso ($resultIMC)";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade grau I ($resultIMC)";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade grau II ($resultIMC)";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade grau III ($resultIMC)";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,69 +59,74 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         backgroundColor: Colors.green,
         actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {},
-          )
+          IconButton(icon: Icon(Icons.refresh), onPressed: _resetFields),
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(
-            Icons.person_outline,
-            size: 120.0,
-            color: Colors.green,
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Peso em Kg",
-              labelStyle: TextStyle(
-                color: Colors.green,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Peso em Kg",
+                  labelStyle: TextStyle(color: Colors.green),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: weightController,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Informe seu peso!";
+                  }
+                },
               ),
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.green,
-              fontSize: 25.0,
-            ),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Altura (cm)",
-              labelStyle: TextStyle(
-                color: Colors.green,
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Altura (cm)",
+                  labelStyle: TextStyle(color: Colors.green),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: heightController,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Informe sua altura!";
+                  }
+                },
               ),
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.green,
-              fontSize: 25.0,
-            ),
-          ),
-          Container(
-            height: 50.0,
-            child: RaisedButton(
-              child: Text(
-                "Calculate",
-                style: TextStyle(color: Colors.white, fontSize: 25.0),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    child: Text(
+                      "Calculate",
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    ),
+                    color: Colors.green,
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _calculate();
+                      }
+                    },
+                  ),
+                ),
               ),
-              color: Colors.green,
-              onPressed: () {},
-            ),
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+              ),
+            ],
           ),
-          Text(
-            "info",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.green,
-              fontSize: 25.0,
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
